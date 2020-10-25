a4132afda741b9e2e60c9f7509d079b22dfcd6fd
Michael Spremulli <michaels.wpi@gmail.com>
2020-10-24 21:55:24 -0400

    Created card and deck classes

--- BlackJack/src/main/java/Card.java
+++ BlackJack/src/main/java/Card.java
@@ -1,2 +1,18 @@
 public class Card {
+    private int number;//1-13
+    private int suit;//1 clubs 2 diamonds, 3 hearts, 4 spades
+
+    public Card(int number, int suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
 }
--- BlackJack/src/main/java/Deck.java
+++ BlackJack/src/main/java/Deck.java
@@ -1,2 +1,18 @@
 public class Deck {
+    ArrayList<Card> deck= new ArrayList<>();
+
+    //default constructor
+    public Deck() {
+        for (int cardNumber = 1; cardNumber < 14; cardNumber++) {
+            for(int suit = 1; suit < 4; suit++){
+                deck.add(new Card(cardNumber, suit));
+            }
+        }
+    }
+
+    public Card drawRandomCard(){
+        int randNum = Math.floor(1 + deck.size() * (Math.random() + 1));
+        return deck.get(randNum);
+    }
+
 }
