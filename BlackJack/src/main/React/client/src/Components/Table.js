cf0e9be112704dd8823a6957c19b2a435918d2cf
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-08 18:49:27 -0500

    created table and card components

---
+++ BlackJack/src/main/React/client/src/Components/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Card} from './Card';
+
+export const Table = () => {
+    return(
+        <div>
+            <Card />
+            <Card />
+        </div>
+    )
+}
